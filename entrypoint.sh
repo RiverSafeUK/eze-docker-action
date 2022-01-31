884cd00c88f4c016b413d6444e0642521cc63344
mgonzales-rs <melissa.gonzales@riversafe.co.uk>
2022-01-31 09:24:39 +0000

    ab#967: add reporter for markdown

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,25 @@ cd /data
 
 echo "" >> .ezerc.toml
 echo "" >> .ezerc.toml
-echo "[sarif.github]" >> .ezerc.toml
-echo "REPORT_FILE = \"reports/eze.sarif\"" >> .ezerc.toml
-echo "" >> .ezerc.toml
-echo "[scan.github]" >> .ezerc.toml
-echo "reporters = [\"sarif:github\"]" >> .ezerc.toml
-echo "" >> .ezerc.toml
-
-
-eze test -s github
-echo ::set-output name=sarif_file::$(cat reports/eze.sarif)
+if [ $INPUT_UPLOAD_METHOD = "sarif" ]
+then
+    echo "printing in SARIF"
+    echo "[sarif.github]" >> .ezerc.toml
+    echo "REPORT_FILE = \"reports/eze.sarif\"" >> .ezerc.toml
+    echo "" >> .ezerc.toml
+    echo "[scan.github]" >> .ezerc.toml
+    echo "reporters = [\"sarif:github\"]" >> .ezerc.toml
+    echo "" >> .ezerc.toml
+    eze test -s github
+    echo ::set-output name=sarif_file::$(cat reports/eze.sarif)
+else
+    echo "printing in MARKDOWN"
+    echo "[markdown.github]" >> .ezerc.toml
+    echo "REPORT_FILE = \"reports/eze.md\"" >> .ezerc.toml
+    echo "" >> .ezerc.toml
+    echo "[scan.github]" >> .ezerc.toml
+    echo "reporters = [\"markdown:github\"]" >> .ezerc.toml
+    echo "" >> .ezerc.toml
+    eze test -s github
+    echo ::set-output name=markdown_file::$(cat reports/eze.md)
+fi
